b41fd9489839bbdc2550db0837be1f4b8842fb85
UlaşDK <udk97@hotmail.com>
2023-03-04 20:10:58 +0300

    functions

--- Functions/index.ts
+++ Functions/index.ts
@@ -17,3 +17,31 @@ function defaultParameter (name : string = "Ulas"){
 defaultParameter() // returns "Hi there Ulas"
 
 defaultParameter("Icardi") // returns "Hi there Icardi"
+
+// --------------------------------------------------------
+
+// void function
+
+const voidFunc =(num:number)=>{
+    Math.pow(num,2)
+}
+
+// when you hover it appears return type void
+voidFunc(4)
+
+// --------------------------------------------------------
+
+// Exercise
+
+// 1
+const twoFer=(name:string="RDJ")=>{
+    return `One for ${name},one for Batman`
+}
+
+// 2
+const isLeapYear = (year : number):boolean=>{
+    if(year % 4 == 0 && year % 100 !=0 || year % 400 == 0){
+        return true
+    }
+    return false
+}
