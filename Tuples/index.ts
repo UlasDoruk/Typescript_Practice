c80679c13b60ba56d0d0a5387b8a0d24eaadcc58
UlaşDK <udk97@hotmail.com>
2023-03-06 21:40:31 +0300

    tuples

---
+++ Tuples/index.ts
@@ -0,0 +1,10 @@
+// Tuples in TS 
+
+let age : [number,number] = [72,27]
+let tupleList : [string,number] = ["Hi",72]
+
+type Color=[string,number]
+let tupleColor : Color= ["red",255]
+tupleColor.push("blue",0) // ?? 
+
+// Enums in TS
