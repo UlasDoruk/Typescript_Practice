1a5e7c4a2cdd54551598fcf89fe710b248f8650d
UlaşDK <udk97@hotmail.com>
2023-03-06 22:17:05 +0300

    enums

---
+++ Enums/index.ts
@@ -0,0 +1,26 @@
+// Enums in Ts 
+ 
+enum Nations  {
+    GONDOR,
+    ROHAN,
+    RİVENDELL,
+    MORDOR,
+    ISENGARD
+}
+
+//console.log(Nations[4])
+
+// function allNations (item:string){
+//     return Nations[item]
+// }
+// console.log(allNations("MORDOR"))
+
+
+// placing const before enums
+
+const enum Name {
+    MARİO,
+    ICARDİ,
+    FERNANDO,
+    MUSLERA
+}
