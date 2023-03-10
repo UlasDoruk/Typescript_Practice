7a196c6767fefcbaa373a33593887950d65a4fb5
UlaşDK <udk97@hotmail.com>
2023-03-10 12:25:38 +0300

    abstract class

--- Class/src/index.ts
+++ Class/src/index.ts
@@ -1,3 +1,10 @@
+// Interface
+
+interface country {
+    country : string,
+    birthPlace ? : string
+}
+
 class Human {
     constructor(
     private  name:string,
@@ -31,6 +38,41 @@ class Human {
 
 const me = new Human("Ulas","Karaman",25)
 
-class SuperHuman extends Human{
+class SuperHuman extends Human implements country{
+    country = "Arg"
+    birthPlace = "Arg"
     public superHuman : boolean = true
 }
+
+const upgradeMe = new SuperHuman("Mario","Icardi",29)
+
+// Abstract class
+
+abstract class Employee {
+    constructor(public name : string, public lastName : string, public hourlyWork : number) {}
+    moneyForHour = 400
+    abstract salary():number
+}
+
+class FullTime extends Employee{
+    constructor(name:string,lastName:string,hourlyWork:number){
+        super(name,lastName,hourlyWork)
+    }
+    salary(): number {
+        return this.hourlyWork * this.moneyForHour
+    }
+}
+
+class ParthTime extends Employee{
+    constructor(name:string,lastName:string,hourlyWork:number){
+        super(name,lastName,hourlyWork)
+    }
+    salary(): number {
+        return this.hourlyWork * this.moneyForHour
+    }
+}
+
+const Ali = new FullTime("Ali","Tonbul",45)
+console.log(Ali.salary())
+const Meltem = new ParthTime("Ali","Cumbul",25)
+console.log(Meltem.salary())
